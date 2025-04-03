b19d85b3ed0f8a9b05d9a3919d3c3d9da3625212
Harsh <harshagarwal81528@gmail.com>
2025-04-03 22:29:49 +0530

    establish MongoDB connection and add mongoose as a dependency

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
+
+const connectToDB = require('./db/db.js');
+connectToDB();
+
 const app = express();
 
 app.use(cors());

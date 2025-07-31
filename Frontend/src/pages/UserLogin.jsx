08be5babd54b1acf548a7b217dc68b276036dadb
Harsh <harshagarwal81528@gmail.com>
2025-07-31 19:34:38 +0530

    feat: enhance UserLogin component with improved layout and updated button styles

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -2,29 +2,40 @@ import React from 'react'
 
 const UserLogin = () => {
   return (
-    <div className="p-6">
-      <form>
-        <h3 className="font-semibold text-xl mb-2">What's Your Email</h3>
-        <input
-          className="border bg-[#eeeeee] w-full text-lg placeholder:text-base border-gray-300 p-2 rounded-md mb-4"
-          required
-          type="text"
-          placeholder="email@example.com"
-        />
-        <h3 className="font-semibold text-xl mb-2">What's Your Password</h3>
-        <input
-          className="border bg-[#eeeeee] w-full text-lg placeholder:text-base border-gray-300 p-2 rounded-md mb-4"
-          required
-          type="password"
-          placeholder="********"
-        />
+    <div className="p-6 h-screen flex flex-col justify-between">
+      <div>
+        <form>
+          <h2 className="text-4xl mb-5 font-bold text-gray-700">Cabby</h2>
+          <h3 className="font-semibold text-xl mb-2">What's Your Email</h3>
+          <input
+            className="border bg-[#eeeeee] w-full text-lg placeholder:text-base border-gray-300 p-2 rounded-md mb-4"
+            required
+            type="text"
+            placeholder="email@example.com"
+          />
+          <h3 className="font-semibold text-xl mb-2">Enter Password</h3>
+          <input
+            className="border bg-[#eeeeee] w-full text-lg placeholder:text-base border-gray-300 p-2 rounded-md mb-4"
+            required
+            type="password"
+            placeholder="********"
+          />
+          <button
+            className="w-full bg-black text-white font-semibold py-3 rounded-lg mt-2 mb-2"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+      <div>
         <button
-          className="w-full bg-black text-white font-semibold py-3 rounded-lg mt-2"
-          type="submit"
+          className="w-full bg-[#10b461] text-white font-semibold py-3 rounded-lg mt-2"
+          
         >
-          Login
+          Signup as Captain
         </button>
-      </form>
+      </div>
     </div>
   );
 }
